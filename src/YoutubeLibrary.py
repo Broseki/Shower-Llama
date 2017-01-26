17473d387bff6bf1d34001ccb921b5a8ed35ff60
Michael Canning <mxc9798@rit.edu>
2017-01-26 15:47:00 -0500

    Total overhaul of the audio engine. Now with threading!

--- src/YoutubeLibrary.py
+++ src/YoutubeLibrary.py
@@ -1,11 +1,42 @@
-from pytube import YouTube
 import os
+import time
+import pafy
+import subprocess
+
+
+def convertTime(timex):
+    timeArray = str(timex).split(":")
+    seconds = 0
+    seconds += int(timeArray[0]) * 60 * 60
+    seconds += int(timeArray[1]) * 60
+    seconds += int(timeArray[2])
+    return seconds
+
+
+def musicTimer(timer, filepath):
+    for x in range(0, timer):
+        time.sleep(1)
+        print x
+
+    os.remove(filepath)
+
+
+
+def play(filepath):
+    os.system("/Applications/VLC.app/Contents/MacOS/VLC " + filepath)
 
 
 def playVideo(url):
-    yt = YouTube(url)
-    yt.set_filename('video')
-    video = yt.get('mp4', '360p')
-    video = yt.get('mp4')
-    video.download('')
-    os.system("open video.mp4")
+    video = pafy.new(url)
+    length = convertTime(video.duration)
+    bestAudio = video.getbestaudio()
+    filepath = "temp." + bestAudio.extension
+    bestAudio.download(filepath=filepath)
+    print length
+    subprocess.Popen(["/Applications/VLC.app/Contents/MacOS/VLC", filepath])
+    print 'ok'
+    musicTimer(length, filepath)
+
+
+
+playVideo('https://www.youtube.com/watch?v=m6NHkHrvWCI')
