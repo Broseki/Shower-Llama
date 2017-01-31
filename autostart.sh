01208d69c9a267b41cebdebec54b1b2c7e4144cd
Michael Canning <mcanning@cavefox.net>
2017-01-30 21:25:19 -0500

    Added autostart shell script

---
+++ autostart.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+lxterminal -e sudo python /home/pi/Documents/Shower-Llama-master/src/Llama-Server.py &
+lxterminal -e python /home/pi/Documents/Shower-Llama-master/src/Llama-Client.py &
